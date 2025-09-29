2dad0ef53527b7624460a7d5960dd27a0078b54a
arnavmishika1 <arnavmishika1@gmail.com>
2025-09-29 11:10:58 +0530

    save cart to localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
-export let cart = [{
+export let cart = JSON.parse(localStorage.getItem('cart')) || [{
   productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity: 2
 }, {
@@ -26,9 +26,9 @@ export function addToCart(productId, quantity) {
       quantity
     });
   }
+
+  saveToStorage();
 }
-
-
 
 export function displayCartMessage(addedMessageTimeoutId, productId) {
   // added message
@@ -61,5 +61,9 @@ export function removeFromCart(productId) {
   });
 
   cart = newCart;
-  console.log(cart);
+  saveToStorage();
+}
+
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
 }

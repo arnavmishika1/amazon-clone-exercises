8f37e87dd2646472683ce8bf25913ab5ae3514c3
arnavmishika1 <arnavmishika1@gmail.com>
2025-10-10 20:19:52 +0530

    Load cart using Promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -4,21 +4,26 @@ import { loadProducts } from "../data/products.js";
 import { loadCart } from "../data/cart.js";
 // import '../data/backend-practice.js';
 
-/*
 new Promise((resolve) => {
   loadProducts(() => {
     resolve();
   });
 }).then(() => {
+  return new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  });
+}).then(() => {
   renderOrderSummary();
   renderPaymentSummary();
 });
-*/
 
-
+/*
 loadProducts(() => {
   loadCart(() => {
     renderOrderSummary();
     renderPaymentSummary();
   });
 });
+*/

1068f9a7cd401502f9373ab5b1f982042f1a6f13
arnavmishika1 <arnavmishika1@gmail.com>
2025-10-10 17:16:59 +0530

    Generate HTTP request and get response back

--- data/cart-class.js
+++ data/cart-class.js
@@ -136,8 +136,8 @@ class Cart {
 export const cart = new Cart('cart-oop');
 const businessCart = new Cart('cart-business');
 
-console.log(cart);
-console.log(businessCart);
+// console.log(cart);
+// console.log(businessCart);
 
-console.log(businessCart instanceof Cart);
+// console.log(businessCart instanceof Cart);
 
